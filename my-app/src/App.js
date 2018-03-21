44a103151890cb74ab681108fbf0a9d1f9890d53
Bill-Windsor <bwindsor888@icloud.com>
2018-03-21 09:24:18 -0700

    commit with updated REACT components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import chars from './dcSuperheroes.json'
+import Wrapper from './components/Wrapper'
+import Scores from './components/Scores'
+import Title from './components/Title'
+import CharCard from './components/CharCard'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        chars: chars,
+        unselectedChars: chars
+    }
+    componentDidMount() {
+    }
+
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selectChar = name => {
+        const findChar = this.state.unselectedChars.find(item => item.breed === breed);
+
+        if(findChar === undefined) {
+            // failure to select a new character
+            this.setState({ 
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                chars: chars,
+                unselectedChars: chars
+            });
+        }
+        else {
+            // success to select a new character
+            const newChars = this.state.unselectedChars.filter(item => item.name !== name);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                chars: chars,
+                unselectedChars: newChars
+            });
+        }
+
+        this.shuffleArray(chars);
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Scores
+                    message={this.state.message}
+                    curScore={this.state.curScore}
+                    topScore={this.state.topScore}
+                />
+                <Title />
+                {
+                    this.state.chars.map(char => (
+                        <CharCard
+                            name={char.name}
+                            image={char.image}
+                            selectChar={this.selectChar} 
+                            curScore={this.state.curScore}
+                        />
+                    ))
+                }
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
